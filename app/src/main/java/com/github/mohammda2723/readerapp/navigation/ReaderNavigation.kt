01cccc7f1cc5e4dc1eab11641ddfc9dc0e4e8897
mohammad2723 <kotlin2723@gmail.com>
2023-07-25 14:52:56 +0330

    Creating The Navigation Composable and NavController and NavHost

--- app/src/main/java/com/github/mohammda2723/readerapp/navigation/ReaderNavigation.kt
+++ app/src/main/java/com/github/mohammda2723/readerapp/navigation/ReaderNavigation.kt
@@ -1,4 +1,46 @@
 package com.github.mohammda2723.readerapp.navigation
 
-class ReaderNavigation {
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+ fun ReaderNavigation() {
+
+     val navController = rememberNavController()
+
+    NavHost(navController = navController , startDestination = ReaderScreens.Splash.name){
+
+        //Splash
+        composable(ReaderScreens.Splash.name){
+
+        }
+        //home
+        composable(ReaderScreens.Home.name){
+
+        }
+        //login
+        composable(ReaderScreens.Login.name){
+
+        }
+        //details
+        composable(ReaderScreens.Details.name){
+
+        }
+        //Search
+        composable(ReaderScreens.Search.name){
+
+        }
+        //Stats
+        composable(ReaderScreens.Stats.name){
+
+        }
+        //Update
+        composable(ReaderScreens.Update.name){
+
+        }
+
+    }
+
 }
