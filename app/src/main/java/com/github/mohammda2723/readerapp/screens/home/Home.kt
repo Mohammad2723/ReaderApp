fad9d5cf9bc551d60c3f8f2fed2bfe36c32bdf1e
mohammad2723 <kotlin2723@gmail.com>
2023-08-08 14:59:21 +0330

    Home screen >>>>>>>>>>> added reading Area

--- app/src/main/java/com/github/mohammda2723/readerapp/screens/home/Home.kt
+++ app/src/main/java/com/github/mohammda2723/readerapp/screens/home/Home.kt
@@ -2,16 +2,20 @@ package com.github.mohammda2723.readerapp.screens.home
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.rounded.Logout
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,16 +31,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.github.mohammda2723.readerapp.model.MBook
 import com.github.mohammda2723.readerapp.navigation.ReaderScreens
 import com.google.firebase.auth.FirebaseAuth
 
 
 @OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+//@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Home(navController: NavController) {
 
@@ -48,14 +57,10 @@ fun Home(navController: NavController) {
                 // todo: onClick FloatingActionButton
             }
         })
-    {
+
+    { padding ->
         // home screen content
-        Column(modifier = Modifier.fillMaxSize() , horizontalAlignment = Alignment.CenterHorizontally) {
-
-
-
-
-        }
+        HomeContent(modifier = Modifier.padding(padding), navController = navController)
     }
 }
 
@@ -88,7 +93,7 @@ fun MyTopAppBar(
 
 //LogoutButton
 @Composable
-fun LogoutButton( onClick: () -> Unit) {
+fun LogoutButton(onClick: () -> Unit) {
     IconButton(onClick = {
         onClick.invoke()
     }) {
@@ -150,3 +155,72 @@ fun MyFloatActionButton(onClick: () -> Unit) {
 
 }
 //endregion TopAppBar SECTION
+
+
+@Composable
+fun HomeContent(modifier: Modifier, navController: NavController) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+
+        ReadingRightNowArea(navController = navController)
+
+    }
+}
+
+@Composable
+fun ReadingRightNowArea(books: List<MBook> = emptyList(), navController: NavController) {
+
+    val name = FirebaseAuth.getInstance()
+        .currentUser?.email?.split('@')?.get(0) ?: "N/A"
+
+
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+
+        TitleSection(label = "You reading \n Activity Right now")
+
+        Column(verticalArrangement = Arrangement.Center) {
+            Icon(
+                modifier = Modifier
+                    .clickable { navController.navigate(ReaderScreens.Stats.name) }
+                    .size(45.dp),
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.primary
+            )
+
+            Text(
+                modifier = Modifier.padding(2.dp),
+                text = name,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.primary,
+                maxLines = 1,
+                overflow = TextOverflow.Clip
+            )
+        }
+    }
+}
+
+@Composable
+fun TitleSection(
+    modifier: Modifier = Modifier,
+    label: String = "Studying: \n Song Of Ice and Fire"
+) {
+
+    Surface(modifier = modifier.padding(start = 5.dp, top = 1.dp)) {
+
+        Column {
+            Text(
+                text = label,
+                fontSize = 19.sp,
+                fontStyle = FontStyle.Normal,
+                textAlign = TextAlign.Left
+            )
+
+        }
+
+    }
+
+
+}
