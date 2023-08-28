5dedd682d842b8e515c6dc69d7f13afa0c94cf4e
mohammad2723 <kotlin2723@gmail.com>
2023-08-28 16:24:41 +0330

    Search Screen >>>>>>>>>> remove SearchViewModel and created  new Repository with a new way to handel error and get data with sealed class Resource

--- app/src/main/java/com/github/mohammda2723/readerapp/data/DataOrException.kt
+++
@@ -1,7 +0,0 @@
-package com.github.mohammda2723.readerapp.data
-
-data class DataOrException<T, Boolean, E : Exception>(
-    var data: T? = null,
-    var loading: kotlin.Boolean? = null,
-    var e: E? = null
-)
--- app/src/main/java/com/github/mohammda2723/readerapp/repository/BookRepository.kt
+++ app/src/main/java/com/github/mohammda2723/readerapp/repository/BookRepository.kt
@@ -1,7 +1,6 @@
 package com.github.mohammda2723.readerapp.repository
 
-import com.github.mohammda2723.readerapp.data.DataOrException
-import com.github.mohammda2723.readerapp.model.Book
+import com.github.mohammda2723.readerapp.data.Resource
 import com.github.mohammda2723.readerapp.model.Item
 import com.github.mohammda2723.readerapp.network.BookApi
 import javax.inject.Inject
@@ -9,42 +8,39 @@ import javax.inject.Inject
 class BookRepository @Inject constructor(private val api: BookApi) {
 
 
-    private val dataOrException = DataOrException<List<Item>, Boolean, Exception>()
-    private val bookInfoDataOrException = DataOrException<Item, Boolean, Exception>()
+    suspend fun getBooks(searchQuery: String): Resource<List<Item>> {
 
-
-    suspend fun getBooks(searchQuery: String): DataOrException<List<Item>, Boolean, Exception> {
-
-        /////////////////////////////DataOrException is a data class that we create
+        /////////////////////////////Resource is a sealed  class that we create
         // for manged error loading and data that came from internet or database
 
-        try {
+        return try {
 
-            dataOrException.loading = true
-            dataOrException.data = api.getAllBooks(searchQuery).items
-            if (dataOrException.data!!.isNotEmpty()) dataOrException.loading = false
+            Resource.Loading(data = true)
+            val itemList = api.getAllBooks(searchQuery).items
+            if (itemList.isNotEmpty()) Resource.Loading(data = false)
+            Resource.Success(data = itemList)
 
-        } catch (e: Exception) {
+        } catch (exception: Exception) {
 
-            dataOrException.e = e
-
+            Resource.Error(massage = exception.message.toString())
         }
-        return dataOrException
+
+
     }
 
 
-    suspend fun getBookInfo(bookId: String): DataOrException<Item, Boolean, Exception> {
+    suspend fun getBook(bookId: String): Resource<Item> {
 
-        try {
-            bookInfoDataOrException.loading = true
-            bookInfoDataOrException.data = api.getBookInfo(bookId)
-            if (bookInfoDataOrException.data.toString().isNotEmpty())
-                bookInfoDataOrException.loading = false
+        val response = try {
+            Resource.Loading(data = true)
+            api.getBookInfo(bookId)
 
         } catch (e: Exception) {
-            bookInfoDataOrException.e = e
-        }
-        return bookInfoDataOrException
 
+            return Resource.Error(massage = e.message.toString())
+        }
+
+        Resource.Loading(data = false)
+        return Resource.Success(data = response)
     }
 }
